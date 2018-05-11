4baccc103e04f21193cc0d3b88c8da2f6fa3242d
Omer Siddiqui <oasiddiqui1@gmail.com>
2018-05-11 11:29:33 -0400

    changed path of links to github pages

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// NAV POPOUT
+
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    //Open menu
+    $hamburger.toggleClass("is-active");
+    // Close menu
+      $('.menu-inner').toggleClass('hide');
+      $('.menu bars').toggleClass('hide');
+  });
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+// TYPEWRITER
+
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -58,7 +75,63 @@ window.onload = function() {
 
 ///////////////////////////////////////////////////////////////
 
+// FLOATING SHAPES-- CIRCLE
+
 function colorChange() {
 	$('.circle').css('border-color', colorsSelected[activeSlide]);
 
 }
+
+////////////////////////////////////////////////////////////
+
+// SMOOTH scroll
+
+$('.smooth a').smoothScroll({
+  offset: 100,
+  speed: 1000
+});
+
+$('.header-text a').smoothScroll({
+  offset: 100,
+  speed: 1000
+});
+
+//////////////////////////////////////////////////////////////////
+
+// CONTACT ANIMATION
+let $contactForm = $('#contact');
+
+$contactForm.waypoint(function() {
+    $contactForm.addClass('animated fadeInDown show');
+  }, {
+    offset: '50%'
+  });
+
+/////////////////////////////////////////////////////////////////////////
+
+// EMAIL VALIDATION
+
+//  Email validation
+function validateEmail(email) {
+  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+// Submission function
+function validate() {
+  let email = $("#email").val();
+
+// Change submit button to green if it validates and sends
+  if (validateEmail(email)) {
+
+    $("button").css("background-color", "green");
+
+// Error message
+  } else {
+    $(".invalid").addClass("show");
+  }
+  return false;
+}
+
+// Run submission function
+$("#submit").on("click", validate);
