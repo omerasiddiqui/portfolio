739645d45855e039ef1e6b46701c27733c57ed7c
Omer Siddiqui <oasiddiqui1@gmail.com>
2018-05-15 14:37:13 -0400

    before redoing data structure for gulp

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 125 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
